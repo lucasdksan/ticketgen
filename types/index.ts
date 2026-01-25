124456b9c3505b4ae1b76a029ad928cca833d84d
lucas <lucas.leoncio.silva@gmail.com>
2026-01-24 22:29:21 -0300

    Nova tipagem por categoria

--- types/index.ts
+++ types/index.ts
@@ -3,15 +3,27 @@ export interface Ticket {
     timestamp: Date;
 }
 
+export interface CategoryCount {
+    name: string;
+    count: number;
+}
+
 export interface SessionData {
     userName: string;
     eventName: string;
     startTime: Date;
-    tickets: Ticket[];
+    mode: SessionMode;
+    tickets?: Ticket[];
+    categories?: CategoryCount[];
 }
 
 export enum AppState {
     SETUP = "SETUP",
     ACTIVE = "ACTIVE",
     SUMMARY = "SUMMARY"
+}
+
+export enum SessionMode {
+    TICKETS = "TICKETS",
+    CATEGORIES = "CATEGORIES"
 }
