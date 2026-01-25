82e9a07afc6f4f4e3c42d9c599dc5eb1c1d4a9e6
lucas <lucas.leoncio.silva@gmail.com>
2026-01-24 21:51:43 -0300

    melhoria de SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -10,6 +10,25 @@ const inter = Inter({
 export const metadata: Metadata = {
   title: "TicketGen",
   description: "Gestão de atendimento profissional",
+  icons: {
+    icon: "/favicon.ico",
+  },
+  creator: "Lucas da Silva Leoncio",
+  publisher: "Lucas da Silva Leoncio",
+  applicationName: "TicketGen",
+  keywords: ["ticket", "gen", "atendimento", "profissional", "gerador de senhas"],
+  authors: [{ name: "Lucas da Silva Leoncio", url: "https://github.com/lucasdksan" }],
+  openGraph: {
+    title: "TicketGen",
+    description: "Gestão de atendimento profissional",
+    url: "https://ticketgen-three.vercel.app/",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "TicketGen",
+    description: "Gestão de atendimento profissional",
+    images: ["https://ticketgen-three.vercel.app/og-image.png"],
+  },
 };
 
 export default function RootLayout({
